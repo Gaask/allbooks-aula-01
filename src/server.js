7ac7ab1e6e25c21be38d4367c45f3be9218af58f
Gaask <gaaskmn@gmail.com>
2026-05-16 11:21:35 -0300

    Atualização local

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
-const bodyParser = require('body-parser')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./src/data/database.json')
-const publicRoutes = require('./routes/publicRoutes')
-const authenticationMiddleware = require('./middleware/authenticationMiddleware')
+  const bodyParser = require('body-parser')
+  const jsonServer = require('json-server')
+  const server = jsonServer.create()
+  const router = jsonServer.router('./src/data/database.json')
+  const publicRoutes = require('./routes/publicRoutes')
+  const authenticationMiddleware = require('./middleware/authenticationMiddleware')
 
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
-server.use(jsonServer.defaults());
+  server.use(bodyParser.urlencoded({ extended: true }))
+  server.use(bodyParser.json())
+  server.use(jsonServer.defaults());
 
-server.use('/public', publicRoutes) //rotas na porta 8000
+  server.use('/public', publicRoutes) //rotas na porta 8000
 
 
-server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
+  server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+  server.use(router) //rotas na prota 3000
 
-server.listen(8000, () => {
-  console.log("Acesse essa API em http://localhost:8000")
-  status::"DEIXA O ERRO DE MERGE ACONTECER MEU DEUS"
-})
+  server.listen(8000, () => {
+    console.log("Adicionada alteração via VS Code para forçar erro de merge")
+    console.log("API disponível através de http://localhost:8000")
+  })
